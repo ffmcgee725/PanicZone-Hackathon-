9b5d492e61b9bcc4c307ac5786ca94d7d83b183d
João Tavares <jc.992@hotmail.com>
2019-08-13 12:53:18 +0000

    libgdx project initiated.

---
+++ desktop/src/org/academiadecodigo/codezillas/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.academiadecodigo.codezillas.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.academiadecodigo.codezillas.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
